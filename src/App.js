7ba920504d373db12f41699e4699d7e69d0d742d
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-23 02:03:08 -0300

    feat: add the Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import GlobalStyle from "./style/GlobalStyle";
+
 function App() {
   return (
-    <>
-      OI
-    </>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
+import {
+  IoSearch,
+  IoPersonCircleSharp,
+  IoHeartSharp,
+  IoBag,
+} from "react-icons/io5";
+
+export default function Header() {
+  return (
+    <Container>
+      <Logo>
+        <img src={logo} alt="logo" />
+      </Logo>
+      <SearchBox>
+        <input type="text" placeholder="search product" />
+        <IoSearch />
+      </SearchBox>
+      <IconBox>
+        <IoHeartSharp />
+        <IoPersonCircleSharp />
+        <IoBag />
+      </IconBox>
+    </Container>
+  );
+}
+
+const Container = styled.header`
+  width: 100vw;
+  height: 180px;
+  background: #000f21;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 80px;
+`;
+const Logo = styled.figure`
+  width: 165px;
+  height: 55px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SearchBox = styled.div`
+  position: relative;
+
+  input {
+    width: 375px;
+    height: 54px;
+    border-radius: 25px;
+    background: #d9d9d9;
+    border: none;
+    padding: 15px;
+    font-size: 18px;
+    :focus {
+      outline: none;
+    }
+  }
+  svg {
+    font-size: 30px;
+    position: relative;
+    right: 50px;
+    top: 10px;
+  }
+`;
+const IconBox = styled.div`
+  width: 180px;
+  display: flex;
+  justify-content: space-between;
+  svg {
+    font-size: 30px;
+    color: #d9d9d9;
+  }
+`;
