37fb292f675920788ddbadb039be930698fe083d
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-11-24 04:07:07 -0300

    feat: LoginPage with local storage and useContext

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import SelectedProductPage from "./pages/SelectedProductPage/SelectedProductPage";
-import GlobalStyle from "./style/GlobalStyle";
+import GlobalStyle from "./assets/style/GlobalStyle";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import { UserProvider } from "./providers/UserData";
 
 function App() {
-  console.log("aaaaap")
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/:productId" element={<SelectedProductPage/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+	console.log("aaaaap");
+	return (
+		<UserProvider>
+			<GlobalStyle />
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/product/:productId" element={<SelectedProductPage />} />
+					<Route path="/signin" element={<LoginPage />} />
+					<Route path="/signup" element={<RegistrationPage />} />
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
+	);
 }
 
 export default App;
