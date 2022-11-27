544d5949a10b7ac9a242be964c1efcc10b11b38e
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-11-27 02:00:47 -0300

    feat: Added Files for creation of UserPage

--- src/App.js
+++ src/App.js
@@ -11,35 +11,32 @@ import StatesContext from "./providers/StatesContext";
 import { useState } from "react";
 import BlackScreen from "./components/BlackScreen/BlackScreen";
 import ChekouPage from "./pages/CheckoutPage/CheckoutPage";
+import UserPage from "./pages/UserPage/UserPage";
 
 function App() {
-  const [showResult, setShowResult] = useState(false);
-  const [showCart, setShowCart] = useState(false);
+	const [showResult, setShowResult] = useState(false);
+	const [showCart, setShowCart] = useState(false);
 
-  return (
-    <UserProvider>
-      <StatesContext.Provider
-        value={{ showCart, setShowCart, showResult, setShowResult }}
-      >
-        <GlobalStyle />
-        <BrowserRouter>
-          {showCart && <Cart />}
-          {showResult && <BlackScreen />}
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-              path="/product/:productId"
-              element={<SelectedProductPage />}
-            />
-            <Route path="/signin" element={<LoginPage />} />
-            <Route path="/signup" element={<RegistrationPage />} />
-            <Route path="/checkout" element={<ChekouPage />} />
-          </Routes>
-        </BrowserRouter>
-      </StatesContext.Provider>
-    </UserProvider>
-  );
+	return (
+		<UserProvider>
+			<StatesContext.Provider value={{ showCart, setShowCart, showResult, setShowResult }}>
+				<GlobalStyle />
+				<BrowserRouter>
+					{showCart && <Cart />}
+					{showResult && <BlackScreen />}
+					<Header />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/product/:productId" element={<SelectedProductPage />} />
+						<Route path="/signin" element={<LoginPage />} />
+						<Route path="/signup" element={<RegistrationPage />} />
+						<Route path="/checkout" element={<ChekouPage />} />
+						<Route path="/user" element={<UserPage />} />
+					</Routes>
+				</BrowserRouter>
+			</StatesContext.Provider>
+		</UserProvider>
+	);
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -2,65 +2,78 @@ import axios from "axios";
 import { BASE_URL } from "../constants/urls";
 
 function createConfig(token) {
-  const config = { headers: { Authorization: `Bearer ${token}` } };
-  return config;
+	const config = { headers: { Authorization: `Bearer ${token}` } };
+	return config;
 }
 
 function getProducts() {
-  const promise = axios.get(`${BASE_URL}/products`);
-  return promise;
+	const promise = axios.get(`${BASE_URL}/products`);
+	return promise;
 }
+
 function getFilteredProducts(name) {
-  const promise = axios.get(`${BASE_URL}/products/filter/${name}`);
-  return promise;
+	const promise = axios.get(`${BASE_URL}/products/filter/${name}`);
+	return promise;
 }
 
 function getSelectedProduct(id) {
-  const promise = axios.get(`${BASE_URL}/products/${id}`);
-  return promise;
+	const promise = axios.get(`${BASE_URL}/products/${id}`);
+	return promise;
 }
 
 function login(loginForm) {
-  const promise = axios.post(`${BASE_URL}/signin`, loginForm);
-  return promise;
+	const promise = axios.post(`${BASE_URL}/signin`, loginForm);
+	return promise;
 }
 
 function register(registerForm) {
-  const promise = axios.post(`${BASE_URL}/signup`, registerForm);
-  return promise;
+	const promise = axios.post(`${BASE_URL}/signup`, registerForm);
+	return promise;
 }
 
 function postProduct(productId, body, token) {
-  const config = createConfig(token);
-  const promise = axios.post(`${BASE_URL}/cart/${productId}`, body, config);
-  return promise;
+	const config = createConfig(token);
+	const promise = axios.post(`${BASE_URL}/cart/${productId}`, body, config);
+	return promise;
 }
+
 function getCartProducts(token) {
-  const config = createConfig(token);
-  const promise = axios.get(`${BASE_URL}/cart`, config);
-  return promise;
+	const config = createConfig(token);
+	const promise = axios.get(`${BASE_URL}/cart`, config);
+	return promise;
 }
+
 function deleCartProduct(id, token) {
-  const config = createConfig(token);
-  const promise = axios.delete(`${BASE_URL}/cart/${id}`, config);
-  return promise;
+	const config = createConfig(token);
+	const promise = axios.delete(`${BASE_URL}/cart/${id}`, config);
+	return promise;
 }
+
 function postPurchaseCompletion(body, token) {
-  const config = createConfig(token);
-  const promise = axios.post(`${BASE_URL}/checkout`, body, config);
-  return promise;
+	const config = createConfig(token);
+	const promise = axios.post(`${BASE_URL}/checkout`, body, config);
+	return promise;
 }
+
+function getPurchaseHistory(token) {
+	const config = createConfig(token);
+
+	const promise = axios.get(`${BASE_URL}/orders`, config);
+
+	return promise;
+}
+
 const api = {
-  createConfig,
-  getProducts,
-  getFilteredProducts,
-  getSelectedProduct,
-  login,
-  register,
-  postProduct,
-  getCartProducts,
-  deleCartProduct,
-  postPurchaseCompletion,
+	createConfig,
+	getProducts,
+	getFilteredProducts,
+	getSelectedProduct,
+	login,
+	register,
+	postProduct,
+	getCartProducts,
+	deleCartProduct,
+	postPurchaseCompletion,
 };
 
 export default api;
